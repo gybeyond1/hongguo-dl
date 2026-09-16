3609427321e897fb7d9bf782d2646a1150823987
gybeyond1 <gybeyond1@users.noreply.github.com>
2026-09-16 17:18:55 +0800

    Fix: simplify codec logging

--- android/app/src/main/java/com/hongguo/dl/MainActivity.java
+++ android/app/src/main/java/com/hongguo/dl/MainActivity.java
@@ -453,13 +453,11 @@ public class MainActivity extends Activity {
                             String mime = fmt.getString(android.media.MediaFormat.KEY_MIME);
                             if (mime.startsWith("video/") && videoTrack < 0) {
                                 videoTrack = i;
-                                dbg("  video: " + mime + " " + fmt.getInt(android.media.MediaFormat.KEY_WIDTH) + "x" + fmt.getInt(android.media.MediaFormat.KEY_HEIGHT) + " br=" + fmt.getInt(android.media.MediaFormat.KEY_BIT_RATE));
+                                dbg("  video: " + mime);
                             }
                             else if (mime.startsWith("audio/") && audioTrack < 0) {
                                 audioTrack = i;
-                                int sr = fmt.contains(android.media.MediaFormat.KEY_SAMPLE_RATE) ? fmt.getInt(android.media.MediaFormat.KEY_SAMPLE_RATE) : 0;
-                                int ch = fmt.contains(android.media.MediaFormat.KEY_CHANNEL_COUNT) ? fmt.getInt(android.media.MediaFormat.KEY_CHANNEL_COUNT) : 0;
-                                dbg("  audio: " + mime + " " + sr + "Hz " + ch + "ch");
+                                dbg("  audio: " + mime);
                             }
                         }
                         dbg("  videoTrack=" + videoTrack + " audioTrack=" + audioTrack);
